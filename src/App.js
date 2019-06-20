a0383dcbf9d52e589d231e1e12bb2f48e04b3eb0
dayavalencia <ariannevalencia@gmail.com>
2019-06-21 05:59:21 +0800

    feat: menu screen

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { Button } from 'antd';
 import dog from './dog.png';
 import './App.css';
+import 'antd/dist/antd.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={dog} className="App-logo" alt="logo" />
-        <Button></Button>
+        <img src={dog} style={{ marginBottom: '10px'}} className="App-logo" alt="logo" />
+        <h1 style={{color: '#fff'}}>Who's Your Pupper?</h1>
+        <Button style={{ marginBottom: '15px'}} shape='round' size='large'>IDENTIFY DOGGO</Button>
+        <Button style={{ marginBottom: '15px'}} shape='round' size='large'> TAKE QUIZ</Button>
+        <Button shape='round' size='large'>MY RESULTS</Button>
       </header>
     </div>
   );
